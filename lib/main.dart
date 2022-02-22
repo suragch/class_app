3e79b63af2270624fb7431ad77979039916379c4
suragch <suragch@suragch.dev>
2022-02-22 10:48:10 +0800

    added button row

--- lib/main.dart
+++ lib/main.dart
@@ -12,46 +12,93 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter layout demo',
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Flutter layout demo'),
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: Column(
+          children: [
+            TitleRow(),
+            Row(
+              children: [
+                ButtonColumn(),
+                ButtonColumn(),
+                ButtonColumn(),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ButtonColumn extends StatelessWidget {
+  const ButtonColumn({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(Icons.call, color: Colors.blue),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            'CALL',
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: Colors.blue,
+            ),
           ),
-          body: Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.all(32),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Column(
-                        children: [
-                          Container(
-                            padding: const EdgeInsets.only(bottom: 8),
-                            child: const Text(
-                              'Oeschinen Lake Campground',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                          Text(
-                            'Kandersteg, Switzerland',
-                            style: TextStyle(
-                              color: Colors.grey[500],
-                            ),
-                          ),
-                        ],
-                      ),
+        ),
+      ],
+    );
+  }
+}
+
+class TitleRow extends StatelessWidget {
+  const TitleRow({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
                     ),
-                    Icon(
-                      Icons.star,
-                      color: Colors.red[500],
-                    ),
-                    const Text('41'),
-                  ],
+                  ),
                 ),
-              )
-            ],
-          )),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
+        ],
+      ),
     );
   }
 }
