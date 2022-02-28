a6cbc997da9c5460a04a2210c1e1bfaa038c78d8
suragch <suragch@suragch.dev>
2022-02-28 10:24:56 +0800

    layout finished

--- lib/tempCodeRunnerFile.dart
+++ lib/tempCodeRunnerFile.dart
@@ -17,58 +17,18 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Image.asset(
-              'images/lake.jpg',
-              width: 600,
-              height: 240,
-              fit: BoxFit.cover,
+            TitleRow(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ButtonColumn(label: 'CALL', icon: Icons.call),
+                ButtonColumn(label: 'ROUTE', icon: Icons.near_me),
+                ButtonColumn(label: 'SHARE', icon: Icons.share),
+              ],
             ),
-            const TitleRow(),
-            const ButtonRow(),
-            const TextSection(),
           ],
         ),
       ),
-    );
-  }
-}
-
-class TextSection extends StatelessWidget {
-  const TextSection({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-        'Alps. Situated 1,578 meters above sea level, it is one of the '
-        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-        'half-hour walk through pastures and pine forest, leads you to the '
-        'lake, which warms to 20 degrees Celsius in the summer. Activities '
-        'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
-    );
-  }
-}
-
-class ButtonRow extends StatelessWidget {
-  const ButtonRow({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        ButtonColumn(label: 'CALL', icon: Icons.call),
-        ButtonColumn(label: 'ROUTE', icon: Icons.near_me),
-        ButtonColumn(label: 'SHARE', icon: Icons.share),
-      ],
     );
   }
 }
