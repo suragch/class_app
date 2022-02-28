bbdce50234cc1d18bbd475577accd77fd29af6c0
suragch <suragch@suragch.dev>
2022-02-28 11:31:25 +0800

    ListView with ListTiles

--- lib/main.dart
+++ lib/main.dart
@@ -15,135 +15,25 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Column(
-          children: [
-            Image.asset(
-              'images/lake.jpg',
-              width: 600,
-              height: 240,
-              fit: BoxFit.cover,
+        body: ListView(
+          children: const [
+            ListTile(
+              title: Text('Ul hodlox'),
+              subtitle: Text('15,373 zar'),
+              leading: Icon(Icons.home),
             ),
-            const TitleRow(),
-            const ButtonRow(),
-            const TextSection(),
+            ListTile(
+              title: Text('Ul hodlox'),
+              subtitle: Text('15,373 zar'),
+              leading: Icon(Icons.home),
+            ),
+            ListTile(
+              title: Text('Ul hodlox'),
+              subtitle: Text('15,373 zar'),
+              leading: Icon(Icons.home),
+            ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class TextSection extends StatelessWidget {
-  const TextSection({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-        'Alps. Situated 1,578 meters above sea level, it is one of the '
-        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-        'half-hour walk through pastures and pine forest, leads you to the '
-        'lake, which warms to 20 degrees Celsius in the summer. Activities '
-        'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
-    );
-  }
-}
-
-class ButtonRow extends StatelessWidget {
-  const ButtonRow({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        ButtonColumn(label: 'CALL', icon: Icons.call),
-        ButtonColumn(label: 'ROUTE', icon: Icons.near_me),
-        ButtonColumn(label: 'SHARE', icon: Icons.share),
-      ],
-    );
-  }
-}
-
-class ButtonColumn extends StatelessWidget {
-  const ButtonColumn({
-    Key? key,
-    required this.label,
-    required this.icon,
-  }) : super(key: key);
-
-  final String label;
-  final IconData icon;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: Colors.blue),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: Colors.blue,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class TitleRow extends StatelessWidget {
-  const TitleRow({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: const Text(
-                    'Oeschinen Lake Campground',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Kandersteg, Switzerland',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const Text('41'),
-        ],
       ),
     );
   }
