2164ea3956dcf1271cdbbd4fac061b22bcb69b96
suragch <suragch@suragch.dev>
2022-03-21 10:31:59 +0800

    navigate to second screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
+import 'package:class_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  final myList = ["hamburger", "khuushuur", "buuz"];
-  print(myList[1]);
-
   runApp(MyApp());
-
-  final student = Student();
-  student.eat();
 }
 
 class MyApp extends StatelessWidget {
@@ -16,62 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.teal),
-      home: HomePage(),
-    );
-  }
-}
-
-class Person {
-  void eat() {
-    print("I am eating.");
-  }
-}
-
-class Student extends Person {
-  @override
-  void eat() {
-    print("The student is eating.");
-  }
-}
-
-class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final countries = [
-    'Albania',
-    'Andorra',
-    'Armenia',
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("My List"),
-      ),
-      body: ListView.builder(
-        itemCount: countries.length,
-        itemBuilder: (context, index) {
-          return Card(
-            elevation: 20,
-            child: ListTile(
-              leading: Icon(Icons.wb_sunny),
-              title: Text(countries[index]),
-              onTap: () {
-                setState(() {
-                  countries.add('Mongol');
-                });
-              },
-            ),
-          );
-        },
-      ),
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,30 @@
+import 'package:class_app/screens/second_screen.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        color: Colors.red.shade100,
+        child: Center(
+          child: ElevatedButton(
+            child: Text('Press me!'),
+            onPressed: () {
+              print('Hello');
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SecondScreen(),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
